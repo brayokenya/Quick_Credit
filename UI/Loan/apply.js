ee588565958fc96b55c2592e5b2ea2a6fa659f54
brayokenya <kiirubrian21@gmail.com>
2019-04-26 01:21:24 +0300

    first Commit

---
+++ UI/Loan/apply.js
@@ -0,0 +1,27 @@
+var amount = document.getElementById(amount).value;
+var income = document.getElementById(income).value;
+
+
+var purpose = document.getElementById(Purpose_of_loan).value;
+var value = purpose.options[purpose.selectIndex].value;
+
+var gender = document.getElementById(Gender_of_Applicant).value;
+var value = gender.options[gender.selectIndex].value;
+
+var first= document.getElementById(first).value;
+var middle = document.getElementById(middle).value;
+var last = document.getElementById(last).value;
+var name = first + " " + middle + " " + last
+
+var birthday = document.getElementById(birthday).value;
+var birthmonth = document.getElementById(birthmonth).value;
+var birthyear = document.getElementById(birthyear).value;
+var DOB = birthday + " " + birthmonth + " " + birthyear
+
+
+var country = document.getElementById(country).value;
+var value = country.options[country.selectIndex].value;
+
+var National_ID_number = document.getElementById(num1).value;
+
+var email = document.getElementById(email).value;
---
+++ UI/Login and Sign Up/login .js
@@ -0,0 +1,5 @@
+var email = document.getElementById(email).value;
+var user = document.getElementById(user).value;
+var name = email + " " + user;
+
+var pass1 = document.getElementById(pass1).value;
