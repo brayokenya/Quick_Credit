ba7e1e0c41b8668a4b2e924d8caf92b23b4be458
Ctrl_Alt_Delicious <kiirubrian21@gmail.com>
2019-05-08 15:43:07 +0300

    Delete login .js

--- Login and Sign Up/login .js
+++
@@ -1,5 +0,0 @@
-var email = document.getElementById(email).value;
-var user = document.getElementById(user).value;
-var name = email + " " + user;
-
-var pass1 = document.getElementById(pass1).value;
