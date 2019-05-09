8f9edbb42789d9894fe01293848b62bc84859bd3
brayokenya <kiirubrian21@gmail.com>
2019-05-09 10:25:39 +0300

    remove unwanted

--- UI/Login and Sign Up/login .js
+++
@@ -1,7 +0,0 @@
-const loginbtn = document.getElementById("loginbtn")
-loginbtn.oneclick = function(){
-
-const Username = document.getElementById(Username).value;
-const Password = document.getElementById(Password).value;
-
-}
