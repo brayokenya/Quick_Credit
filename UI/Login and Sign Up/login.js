0be62abb189357f96711c61dec60fbee32eddef9
brayokenya <kiirubrian21@gmail.com>
2019-05-13 15:37:27 +0200

    login.js update

--- UI/Login and Sign Up/login.js
+++ UI/Login and Sign Up/login.js
@@ -2,8 +2,4 @@ const email = document.getElementById(email).value;
 const user = document.getElementById(user).value;
 const name = email + " " + user;
 
-<<<<<<< HEAD:UI/Login and Sign Up/login .js
 const pass1 = document.getElementById(pass1).value;
-=======
-var pass1 = document.getElementById(pass1).value;
->>>>>>> 6a49d7ccb4b82e2262ec73c2d69e45184b708bdf:UI/Login and Sign Up/login.js
