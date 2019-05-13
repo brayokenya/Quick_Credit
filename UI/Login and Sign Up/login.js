c576a6c5e5e5ff45587d59ba715f30d89f3e2e02
brayokenya <kiirubrian21@gmail.com>
2019-05-13 16:27:06 +0200

    fixed data capturing on login form

--- UI/Login and Sign Up/login.js
+++ UI/Login and Sign Up/login.js
@@ -3,3 +3,7 @@ const user = document.getElementById(user).value;
 const name = email + " " + user;
 
 const pass1 = document.getElementById(pass1).value;
+const form = document.querySelector('form');
+fetch('endpoint',{
+  body: new URLSearchParams(new FormData(form))
+})
