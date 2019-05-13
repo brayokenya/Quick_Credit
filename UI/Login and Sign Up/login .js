080c4647cba21021e85235171433d0d8fd425816
brayokenya <kiirubrian21@gmail.com>
2019-05-13 12:57:28 +0200

    UI update

--- UI/Login and Sign Up/login .js
+++
@@ -1,5 +0,0 @@
-const email = document.getElementById(email).value;
-const user = document.getElementById(user).value;
-const name = email + " " + user;
-
-const pass1 = document.getElementById(pass1).value;
