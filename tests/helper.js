ccce3c23cbe3d0e58456c972fbe458904d29fa48
Ctrl_Alt_Delicious <kiirubrian21@gmail.com>
2019-05-14 11:49:48 +0200

    Update helper.js

--- tests/helper.js
+++ tests/helper.js
@@ -5,15 +5,15 @@ const { hashPassword, comparePassword } = require('../helper/helper');
 describe('JWT, BCRYPT', () => {
   let result;
   it('Should return hashed password', () => {
-    result = hashPassword('123456');
+    result = hashPassword('12345');
     expect(result).to.be.a('string');
   });
   it('Should return true if password matches hashed', () => {
-    result = comparePassword(`${result}`, '123456');
+    result = comparePassword(`${result}`, '12345');
     expect(result).to.be.equal(true);
   });
   it('Should return false if password does not matches hashed', () => {
-    result = comparePassword(`${result}`, '12345');
+    result = comparePassword(`${result}`, '1234');
     expect(result).to.be.equal(false);
   });
 });
