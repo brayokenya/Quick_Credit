807ca7dd4bdb43ffb44e21d77ceff3f219d9dbd0
brayokenya <kiirubrian21@gmail.com>
2019-05-17 14:38:52 +0200

    few updates

--- src/app.js
+++ src/app.js
@@ -12,8 +12,8 @@ app.use('/api/v1/auth', signup);
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Welcome to QuickCredit' });
 });
-app.use('/api/v1/auth/', signup);
- res.status(200).json({ message: "Enter login details"});
+//app.use('/api/v1/auth/', signup);
+// res.status(400).json({ message: "Enter login details"});
 
 app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
 
